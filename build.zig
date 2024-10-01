36e5576466d36e727697690d1675142b832fa725
noxabellus <noxabellus@gmail.com>
2024-10-01 14:23:36 -0500

    change build script for compatibility with embedding in another

--- build.zig
+++ build.zig
@@ -6,43 +6,47 @@ pub const Module = @import("src/root.zig");
 pub fn build(b: *Build) !void {
     const defaultTarget = b.standardTargetOptions(.{});
     const defaultOptimize = b.standardOptimizeOption(.{});
+    const path = b.path(".");
 
+    return buildSub(b, path, defaultTarget, defaultOptimize);
+}
 
+pub fn buildSub(b: *Build, path: Build.LazyPath, defaultTarget: Build.ResolvedTarget, defaultOptimize: std.builtin.OptimizeMode) !void {
     const Templater = b.addExecutable(.{
         .name = "templater",
-        .root_source_file = b.path("src/bin/Templater.zig"),
+        .root_source_file = path.path(b, "src/bin/Templater.zig"),
         .target = defaultTarget,
         .optimize = defaultOptimize,
     });
 
     const LibJoiner = b.addExecutable(.{
         .name = "libjoiner",
-        .root_source_file = b.path("src/bin/LibJoiner.zig"),
+        .root_source_file = path.path(b, "src/bin/LibJoiner.zig"),
         .target = defaultTarget,
         .optimize = defaultOptimize,
     });
 
     _ = b.addModule("HeaderGenUtils", .{
-        .root_source_file = b.path("src/HeaderGenUtils.zig"),
+        .root_source_file = path.path(b, "src/HeaderGenUtils.zig"),
         .target = defaultTarget,
         .optimize = defaultOptimize,
     });
 
     _ = b.addModule("Manifest", .{
-        .root_source_file = b.path("src/Manifest.zig"),
+        .root_source_file = path.path(b, "src/Manifest.zig"),
         .target = defaultTarget,
         .optimize = defaultOptimize,
     });
 
     const Snapshot = b.addModule("Snapshot", .{
-        .root_source_file = b.path("src/Snapshot.zig"),
+        .root_source_file = path.path(b, "src/Snapshot.zig"),
         .target = defaultTarget,
         .optimize = defaultOptimize,
     });
 
     const SnapshotWriter = b.addExecutable(.{
         .name = "snapshot-writer",
-        .root_source_file = b.path("src/bin/SnapshotWriter.zig"),
+        .root_source_file = path.path(b, "src/bin/SnapshotWriter.zig"),
         .target = defaultTarget,
         .optimize = defaultOptimize,
     });
